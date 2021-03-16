df5897ac785b169566f1593d91c817b080f8f225
Webkadiz <webkadiz@gmail.com>
2021-03-16 21:41:44 +0300

    change service for parsing. instead google used now price.ru

--- backend/google-client.js
+++ backend/google-client.js
@@ -1,20 +1,17 @@
 const axios = require("axios")
 
 const googleClient = axios.create({
-	baseURL: "https://google.com/search",
+	baseURL: "https://price.ru/search",
 	timeout: 5000,
 	headers: {
 		"user-agent":
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
 		"accept-language": "en,ru-RU;q=0.9,ru;q=0.8"
-	},
-	params: {
-		tbm: "shop"
 	}
 })
 
 const genParamsForSearch = (textForSearch) => {
-	return { params: { q: textForSearch } }
+	return { params: { query: textForSearch } }
 }
 
 module.exports = {
--- backend/google-products-parser.js
+++ backend/google-products-parser.js
@@ -28,39 +28,40 @@ class GoogleGoodsParser {
 	}
 
 	#parseProducts() {
-		const pageTypes = ["common", "brand"]
-		const contentSelectors = [".sh-dlr__content", ".sh-dgr__content"]
-		let productsContent, pageType
+		const contentSelectors = ["article"]
+		let productsContent
 
 		for (let i = 0; i < contentSelectors.length; i++) {
-			pageType = pageTypes[i]
-			productsContent = this.#document.querySelectorAll(contentSelectors[i])
+			productsContent = this.#document.querySelectorAll(
+				contentSelectors[i]
+			)
 
 			if (productsContent.length) break
 		}
 
 		if (productsContent?.length) {
 			return Array.from(productsContent).map(
-				this.#parseProduct.bind(this, pageType)
+				this.#parseProduct.bind(this)
 			)
 		} else {
 			return []
 		}
 	}
 
-	#parseProduct(type, productContent) {
+	#parseProduct(productContent) {
 		const product = {}
 		const processFuncs = [
 			this.#processTitleText,
 			this.#processImgSrc,
 			this.#processPrice,
+			this.#processShopCount,
 			this.#processSiteName,
 			this.#processSiteLink
 		]
 
 		for (const processFunc of processFuncs) {
 			try {
-				const [key, value] = processFunc(type, productContent)
+				const [key, value] = processFunc(productContent)
 
 				product[key] = value
 			} catch (e) {}
@@ -69,64 +70,51 @@ class GoogleGoodsParser {
 		return product
 	}
 
-	#processTitleText(type, productContent) {
-		const productTitle = productContent.querySelector("h4, h3")
+	#processTitleText(productContent) {
+		const productTitle = productContent.querySelector(".b-item__title")
 		const productTitleText = productTitle.textContent
 
 		return ["productTitleText", productTitleText]
 	}
 
-	#processImgSrc(type, productContent) {
-		const productImg = productContent.querySelector("img")
+	#processImgSrc(productContent) {
+		const productImg = productContent.querySelectorAll("img")[1]
 		const productImgSrc = productImg.src
 
 		return ["productImgSrc", productImgSrc]
 	}
 
-	#processPrice(type, productContent) {
-		const productPrice = productContent.querySelector("span span")
+	#processPrice(productContent) {
+		const productPrice = productContent.querySelector(".b-price")
 			.textContent
 
 		return ["productPrice", productPrice]
 	}
 
-	#processSiteName(type, productContent) {
-		if (type === "common") {
-			const productPriceContainer = productContent.querySelector("span")
-			const productSiteName =
-				productPriceContainer.nextElementSibling.textContent
+	#processShopCount(productContent) {
+		const productShopCount = productContent.querySelector(
+			".b-item__link-shop"
+		)
+		const productShopCountText = productShopCount.textContent
 
-			return ["productSiteName", productSiteName]
-		} else if (type === "brand") {
-			const productOffer = productContent.querySelector(
-				".sh-dgr__offer-content"
-			)
-			const productSiteName = productOffer.querySelector("a").textContent
-
-			return ["productSiteName", productSiteName]
-		}
+		return ["productShopCount", productShopCountText]
 	}
 
-	#processSiteLink(type, productContent) {
-		if (type === "common") {
-			const productPriceContainer = productContent.querySelector("span")
-			let productSiteLink =
-				productPriceContainer.nextElementSibling.href
-			
-			productSiteLink = "https://www.google.com" + productSiteLink
+	#processSiteName(productContent) {
+		const productPriceContainer = productContent.querySelector("span")
+		const productSiteName =
+			productPriceContainer.nextElementSibling.textContent
 
-			return ["productSiteLink", productSiteLink]
-		} else if (type === "brand") {
-			const productOffer = productContent.querySelector(
-				".sh-dgr__offer-content"
-			)
-			let productSiteLink = productOffer.querySelector("a").href
-			
-			productSiteLink = "https://www.google.com" + productSiteLink
-			console.log(productSiteLink)
+		return ["productSiteName", productSiteName]
+	}
 
-			return ["productSiteLink", productSiteLink]
-		}
+	#processSiteLink(productContent) {
+		const productPriceContainer = productContent.querySelector("span")
+		let productSiteLink = productPriceContainer.nextElementSibling.href
+
+		productSiteLink = "https://www.google.com" + productSiteLink
+
+		return ["productSiteLink", productSiteLink]
 	}
 }
 
